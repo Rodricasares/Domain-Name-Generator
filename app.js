51d069a9833653a8679ec2b534f5d157ef8337b2
Rodricasares <rodrigocantudo@gmail.com>
2022-01-24 22:45:54 +0000

    Listo para corregir

---
+++ app.js
@@ -0,0 +1,19 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let domain=['.com','.org','.net','.us','.io'];
+
+for(let i=0;i<pronoun.length;i++){
+for(let j=0; j<adj.length;j++){
+for(let y=0;y<noun.length;y++){
+for(let x=0;x<domain.length;x++){
+
+console.log(pronoun[i]+adj[j]+noun[y]+domain[x])
+
+    }
+
+} 
+  
+}
+
+}
